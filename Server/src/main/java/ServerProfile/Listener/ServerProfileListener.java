16a4f227e20390094c095cc2e1f48571f9bc0777
Mahdi <mahdid.m.2000@gmail.com>
2021-07-15 16:01:54 +0430

    a little bug fix in change profile pic

--- Server/src/main/java/ServerProfile/Listener/ServerProfileListener.java
+++ Server/src/main/java/ServerProfile/Listener/ServerProfileListener.java
@@ -1,6 +1,7 @@
 package ServerProfile.Listener;
 
 import Connection.Client.ClientRequest;
+import Connection.DataBaseConnection.ConnectionToDataBase;
 import Connection.Server.ServerConnection;
 import ServerConfig.PathConfig.PathConfig;
 
@@ -9,6 +10,8 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.sql.SQLException;
+
 import ServerConstants.*;
 
 public class ServerProfileListener {
@@ -17,16 +20,23 @@ public class ServerProfileListener {
         this.serverConnection = serverConnection;
     }
 
-    public void listen(ClientRequest clientRequest) throws IOException {
+    public void listen(ClientRequest clientRequest) throws IOException, SQLException {
         if(clientRequest.getCommand().equals("changeProfilePic")){
 
             PathConfig pathConfig = new PathConfig();
             BufferedImage bImage = null;
 
             bImage = ImageIO.read(clientRequest.getClientPayLoad().getFile());
-
-            ImageIO.write(bImage, "PNG", new File(pathConfig.getUsersPicPath() +"//"+ clientRequest.getClientPayLoad().getUser().getUserUUID()+".PNG"));
+            String path = pathConfig.getUsersPicPath() +"//"+ clientRequest.getClientPayLoad().getUser().getUserUUID()+".PNG";
+            ImageIO.write(bImage, "PNG", new File(path));
+            saveNewProfilePathToDataBase(clientRequest.getUsername(),path);
 
         }
+    }
+
+    private void saveNewProfilePathToDataBase(String username, String path) throws SQLException {
+        ConnectionToDataBase connectionToDataBase = new ConnectionToDataBase();
+        String sql = String.format("update \"UsersTable\" set \"ProfilePic\" = '%s' where \"UserName\" = '%s';",path,username);
+        connectionToDataBase.executeUpdate(sql);
     }
 }
